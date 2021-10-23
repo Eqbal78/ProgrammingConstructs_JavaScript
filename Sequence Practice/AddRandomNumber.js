675b4fbdc8adf06953a99ba57be11324573d5a25
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-23 12:06:11 +0530

    Two Dice Roll Randomly and Add it

---
+++ Sequence Practice/AddRandomNumber.js
@@ -0,0 +1,6 @@
+// 3. Add two random dice number and print the result
+
+let a = Math.floor(Math.random() * 6) + 1;
+let b = Math.floor(Math.random() * 6) + 1;
+
+console.log("Addition of " + a + " and " + b + " : " + (a + b));
